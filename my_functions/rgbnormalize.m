a736bc04a5f8762aa55d57d28472b3703e6bfe76
Moataz-E <motaz_1993@hotmail.com>
2014-02-03 23:25:08 +0000

    Added rgb normalize script. Added empty soon to be coded main and subtract background scripts.

---
+++ my_functions/rgbnormalize.m
@@ -0,0 +1,22 @@
+function output = rgbnormalize(image_uint)
+    %Given an image, this function returns the an rgb normalized version
+    %of the image.
+ 
+   image = double(image_uint);
+   [H,W,D] = size(image);
+   %Temporary holder initialized to zeros
+   temp = zeros(H,W,D);
+   
+   %Loop going through every pixel in the image
+   for i=1:H
+       for j=1:W
+           %Find average of all colours for
+           %that pixel
+           csum = image(i,j,1) + image(i,j,2) + image(i,j,3);
+           temp(i,j,1) = (image(i,j,1) / csum);
+           temp(i,j,2) = (image(i,j,2) / csum);
+           temp(i,j,3) = (image(i,j,3) / csum);
+       end
+   end
+   output = uint8(temp .* 255);
+end
