deab46cd412a897cf2145b38f721e3e0f4a65e68
Moataz-E <motaz_1993@hotmail.com>
2014-02-07 06:02:14 +0000

    Seperated background subtraction into its own function.

--- my_functions/sub_background.m
+++ my_functions/sub_background.m
@@ -1,9 +1,36 @@
-function diff_image = sub_background(image1, image2)
-    %Given two images, this function attempts to subtract common pixels
-    % from both images, returning an image with the differing pixels.
+function [diffImage, binaryDiffImage] = sub_background(image1, image2, ...
+                                            diffThreshold)
+    %Given two images and a threshold, this function attempts to subtract 
+    %common pixels from both images, returning the difference image in RGB 
+    %format and in binary format.
     %
     %image1 is usually a background image and image2 is the one containing
-    %objects you are after
+    %objects you are after.
+    %
+    %Both images must be of the same size.
    
-    %TO BE COMPLETED
+    %Find image dimensions
+    [height, width, depth] = size(image1);
+    
+    %Find the difference in values between the two images
+    diffValues = sum(abs(image1 - image2), 3);
+    
+	%Matrix to hold resulting background subtracted image
+    diffImage = uint8(zeros(height, width, 3));
+    binaryDiffImage = zeros(height, width);
+    
+    %Populate difference matrices
+    for iColumn = 1 : width
+        for iRow = 1 : height
+            
+            %If value greater than threshold, that means a non-background
+            %object was detected.
+            if diffValues(iRow, iColumn) >= diffThreshold
+               %Store results in both unit8 format and binary format
+               diffImage(iRow, iColumn, :) = ...
+                    image2(iRow, iColumn, :);
+               binaryDiffImage(iRow, iColumn) = 1;
+            end
+        end
+    end  
 end
