10b37242fdbdb7a47c2da44414bd85e9cd57a09f
monicapaun <paun.mona@gmail.com>
2014-02-12 23:57:28 +0000

    Blub of two balls splitting.

--- my_classes/myImage.m
+++ my_classes/myImage.m
@@ -99,31 +99,111 @@ classdef myImage
                 obj.CC = bwconncomp(obj.preprocessed);
 
                 %Find center of mass of all marbles
-                com = regionprops(obj.CC,'Centroid');
+                com = regionprops(obj.CC,'Centroid', 'Area', 'PixelList');
 
                 %Loop through each connected components, identifying and
                 %initializing marble objects
+                id = 1; 
                 for cc = 1 : size(obj.CC.PixelIdxList, 2)
+                    % If the area is too big, assume the connected
+                    % component might represent two/more collidin balls.
+                    area = com(cc).Area;
+                    if area > 400
+                       % Do histogram.
+                       pixel_list = com(cc).PixelList;
+                       [a,~] = size(pixel_list);
+                       bw_img = zeros(obj.height, obj.width);
+                       
+                       for iter = 1 : a
+                           bw_img(pixel_list(iter, 2), pixel_list(iter, 1)) = ...
+                                sum(obj.data(pixel_list(iter, 2), pixel_list(iter, 1), :)) / 3;
+                       end
+                       
+                       bw_array = reshape(bw_img, obj.height*obj.width, 1);
+                       bw_array(bw_array==0) = [];
+                       first_hist = hist(bw_array, 50);
+                       
+                       filter = gausswin(50, 6);
+                       filter = filter/sum(filter);
+                       smooth_hist = conv(filter,first_hist);
+                       
+                       % Find the valley between the two highest peaks.
+                       inv_hist = 1.01*max(smooth_hist) - smooth_hist;
+                       [~, locsmin] = findpeaks(inv_hist);
+                       
+                       % Distribute points between the peaks.
+                       if (length(locsmin) == 1)
+                           tresh = locsmin;
+                           bin1x = 0;
+                           bin1y = 0;
+                           bin2x = 0;
+                           bin2y = 0;
+                           sum1num = 0;
+                           sum2num = 0;
+                           for iter = 1 : a
+                               aux = sum(obj.data(pixel_list(iter, 2), pixel_list(iter, 1), :)) / 3;
+                               if (aux > tresh)
+                                   sum1num = sum1num + 1;
+                                   bin1x = bin1x  +pixel_list(iter, 2);
+                                   bin1y = bin1y  +pixel_list(iter, 1);
+                               else
+                                   sum2num = sum2num + 1;
+                                   bin2x = bin2x  +pixel_list(iter, 2);
+                                   bin2y = bin2y  +pixel_list(iter, 1);
+                               end 
+                           end
+                           bin1x = bin1x / sum1num;
+                           bin1y = bin1y / sum1num;
+                           
+                           bin2x = bin2x / sum2num;
+                           bin2y = bin2y / sum2num;
+                           
+                           dist = ((bin2x - bin1x)^2 + (bin2y - bin1y)^2)^0.5;
+                           
+                           if (dist > 8)
+                               marble = myMarble();
+                               marble = marble.assignID((obj.number*100) + id);
+                               marble = marble.assignCOM([bin1y, bin1x]);
+                               obj.marbles(id) = marble;
+                               id = id + 1;
 
-                    %Marble id is the number of the image at which it was
-                    %identified concatenated to the order at which it is
-                    %identified in a given image
-                    marble = myMarble();
-                    marble = marble.assignID((obj.number*100) + cc);
-                    
-                    %Assign the center of mass of marble
-                    marble = marble.assignCOM(com(cc).Centroid);
-                    
-                    %Loop through each existant marble object and check for
-                    %duplicate ID.
-                    for marbleNum = 1 : size(obj.marbles,2)
-                        
-                        if (marble.ID == obj.marbles(marbleNum).ID)
-                            error('Marble with that ID already exists!');
+                               marble = myMarble();
+                               marble = marble.assignID((obj.number*100) + id);
+                               marble = marble.assignCOM([bin2y, bin2x]);
+                               obj.marbles(id) = marble;
+                               id = id + 1;   
+                           else
+                               marble = myMarble();
+                               marble = marble.assignID((obj.number*100) + id);
+                               marble = marble.assignCOM([(bin1y+bin2y)/2, (bin1x+bin2x)/2]);
+                               obj.marbles(id) = marble;
+                               id = id + 1;
+                           end
+                       end
+                    else
+                        %Marble id is the number of the image at which it was
+                        %identified concatenated to the order at which it is
+                        %identified in a given image
+                        marble = myMarble();
+                        marble = marble.assignID(obj.number*100 + id);
+
+
+                        %Assign the center of mass of marble
+                        marble = marble.assignCOM(com(cc).Centroid);
+
+                        %Loop through each existant marble object and check for
+                        %duplicate ID.
+                        for marbleNum = 1 : size(obj.marbles,2)
+
+                            if (marble.ID == obj.marbles(marbleNum).ID)
+                                error('Marble with that ID already exists!');
+                            end
                         end
+                        %Add this marble to our list of detected marbles
+                        obj.marbles(id) = marble;
+                        id = id + 1;
                     end
-                    %Add this marble to our list of detected marbles
-                    obj.marbles(cc) = marble;
+                    
                 end
                 
             end
